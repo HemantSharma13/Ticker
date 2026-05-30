51ca11587fb47c4dc78638e8c3ecec6a6a3a8e23
HemantSharma13 <hemant.13.sharma.13@gmail.com>
2026-05-30 20:09:08 +0530

    Navigation Menu Created - Landing Page

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,5 +1,21 @@
+import styles from "./LandingPage.module.css";
+
 function LandingPage() {
-  return <div>Landing Page</div>;
+  return (
+    <main>
+      <header className={styles.navbar}>
+        <h2 className={styles.logo}>Ticker</h2>
+
+        <nav className={styles.navLinks}>
+          <a href="#features">Features</a>
+          <a href="#pricing">Pricing</a>
+          <a href="#about">About</a>
+        </nav>
+
+        <button className={styles.loginBtn}>Login</button>
+      </header>
+    </main>
+  );
 }
 
 export default LandingPage;
