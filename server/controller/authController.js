90cf37e11ec8948f05ae72ea4ce579ee3e50baf7
HemantSharma13 <hemant.13.sharma.13@gmail.com>
2026-06-02 22:28:07 +0530

    Adds protect functionality

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -152,3 +152,43 @@ export const logout = (req, res) => {
     message: "Logged out successfully",
   });
 };
+
+export const protect = async (req, res, next) => {
+  try {
+    // 1. Get token from cookie
+    const token = req.cookies.jwt;
+
+    if (!token) {
+      return res.status(401).json({
+        status: "fail",
+        message: "You are not logged in",
+      });
+    }
+
+    // 2. Verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    console.log("decoded value of token:", decoded);
+
+    // 3. Check if user still exists
+    const currentUser = await User.findById(decoded.id);
+
+    if (!currentUser) {
+      return res.status(401).json({
+        status: "fail",
+        message: "User no longer exists",
+      });
+    }
+
+    // 4. Attach user to request
+    req.user = currentUser;
+
+    // 5. Continue
+    next();
+  } catch (err) {
+    return res.status(401).json({
+      status: "fail",
+      message: "Invalid or expired token",
+    });
+  }
+};
