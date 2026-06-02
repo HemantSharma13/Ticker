e7d0a693ee084b03078bc8c282170e681d70b846
HemantSharma13 <hemant.13.sharma.13@gmail.com>
2026-06-02 16:51:12 +0530

    Adds and Configures MongoDB Database with mongoose

--- server/server.js
+++ server/server.js
@@ -1,6 +1,19 @@
 import app from "./app.js";
+import { configDotenv } from "dotenv";
+import mongoose from "mongoose";
 
-const port = process.env.PORT || 3000;
+configDotenv({ path: "./config.env" }); //configuring dotenv
+console.log("The port is", process.env.PORT);
+const port = process.env.SERVER_PORT_NUMBER || 3000;
+
+mongoose
+  .connect(
+    process.env.MONGO_CONNECTION_STRING.replace(
+      "<PASSWORD>",
+      process.env.MONGO_PASSWORD,
+    ),
+  )
+  .then(() => console.log("DB connection successful!"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
