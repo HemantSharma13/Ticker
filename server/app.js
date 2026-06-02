2e8cb110fc6a7d6042d15e49bf391d62416de219
HemantSharma13 <hemant.13.sharma.13@gmail.com>
2026-06-02 20:11:56 +0530

    Adds basic checks and functionality of Signup

--- server/app.js
+++ server/app.js
@@ -1,11 +1,19 @@
 import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
+
+app.use((req, res, next) => {
+  console.log("The request body is:", req.body);
+  next();
+});
+
 app.use("/auth", authRoutes);
 
 app.get("/", (req, res) => {
--- server/controller/authController.js
+++ server/controller/authController.js
@@ -1,4 +1,81 @@
-export function signup(req, res) {
-  console.log("This is the signup req body:", req.body);
-  res.send("Hi, We have received the input!!!");
-}
+// controllers/authController.js
+
+import User from "../model/userModel.js";
+import jwt from "jsonwebtoken";
+
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
+const createSendToken = (user, statusCode, req, res) => {
+  const token = signToken(user._id);
+  console.log("the Token is:", token);
+  res.cookie("jwt", token, {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+    ),
+  });
+};
+
+export const signup = async (req, res) => {
+  try {
+    console.log("req body in signup is", req.body);
+    const { name, email, password, confirm_password } = req.body;
+
+    // Check for empty fields
+    if (!name || !email || !password || !confirm_password) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+
+    // Check password match
+    if (password !== confirm_password) {
+      return res.status(400).json({
+        success: false,
+        message: "Passwords do not match",
+      });
+    }
+
+    // Check if user already exists
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(409).json({
+        success: false,
+        message: "User already exists with this email",
+      });
+    }
+
+    // Create user
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    // Generating JWT Token and directly Logging In
+    createSendToken(user, 200, req, res);
+
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        token: "",
+      },
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
