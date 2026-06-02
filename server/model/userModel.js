bb216c9c5e5cb6db40b9ecad51dc56efbac21797
HemantSharma13 <hemant.13.sharma.13@gmail.com>
2026-06-02 18:33:07 +0530

    User Model Created

---
+++ server/model/userModel.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter your name"],
+    trim: true,
+  },
+
+  email: {
+    type: String,
+    required: [true, "Please enter your email"],
+    unique: true,
+    lowercase: true,
+    match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Please enter a valid email address"],
+  },
+
+  password: {
+    type: String,
+    required: [true, "Please enter a password"],
+    minlength: [10, "Password must be at least 10 characters long"],
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
