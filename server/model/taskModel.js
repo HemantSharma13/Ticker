5784d522c1fed0433829ad627eeb6406bbf16b8f
HemantSharma13 <hemant.13.sharma.13@gmail.com>
2026-06-03 18:43:45 +0530

    Adds Task Model

---
+++ server/model/taskModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Task title is required"],
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      trim: true,
+    },
+
+    status: {
+      type: String,
+      enum: ["Pending", "In Progress", "Completed"],
+      default: "Pending",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Task = mongoose.model("Task", taskSchema);
+
+export default Task;
