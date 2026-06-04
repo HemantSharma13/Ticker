b83ff69b95e4fe2325a8000797de33d6ae276355
HemantSharma13 <hemant.13.sharma.13@gmail.com>
2026-06-04 14:48:47 +0530

    Fix getme function and its routes

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("api/v1/auth", authRoutes);
-app.use("api/v1/tasks", taskRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/tasks", taskRoutes);
 
 export default app;
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 router.get("/logout", authController.logout);
-router.get("/me", authController.me);
+
+router.get("/me", authController.protect, authController.getMe);
 
 export default router;
